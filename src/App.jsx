22a61d252ae4940b478284801ebc74e3d530f519
karinadl <karinadeleonf@gmail.com>
2023-11-19 22:29:04 -0600

    Se agrega el proyecto react-cards

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// //Importando el componente Card
+// import Card from './components/Card';
+// const App = () => {
+//   return <Card />;
+// };
+// export default App;
+
+//Usando el componente Cards
+import Cards from './components/Cards';
+const App = () => {
+  return <Cards />;
+};
+export default App;
